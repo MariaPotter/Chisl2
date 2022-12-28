7f3ca5d73aac91e5f7bff99ac372b6064f852353
MariaPotter <mari.gorshkova.1147@mail.ru>
2022-12-28 22:06:56 +0300

    Finish

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ static double x_d  (double t, double x, double y, double p_x, double p_y, double
 
 static double y_d (double t, double x, double y, double p_x, double p_y, double mult)
 {
-    return p_y/2.-sqrt(2.)*x*exp(-mult*t);
+    return p_y*0.5-sqrt(2.)*x*exp(-mult*t);
 }
 
 static double px_d(double t, double x, double y, double p_x, double p_y, double mult)
@@ -25,6 +25,11 @@ static double px_d(double t, double x, double y, double p_x, double p_y, double 
 static double py_d (double t, double x, double y, double p_x, double p_y, double mult)
 {
     return -p_x;
+}
+
+static double B_d  (double t, double x, double y, double p_x, double p_y, double mult)        
+{
+    return pow(x,2) + pow(p_y*0.5,2);
 }
 
 static double test_x_d  (double t, double x, double y, double p_x, double p_y, double mult)        
@@ -45,6 +50,11 @@ static double test_px_d(double t, double x, double y, double p_x, double p_y, do
 static double test_py_d (double t, double x, double y, double p_x, double p_y, double mult)
 {
     return p_x;
+}
+
+static double test_B_d  (double t, double x, double y, double p_x, double p_y, double mult)        
+{
+    return sqrt(pow(x-p_x,2) + pow(y-p_y,2));
 }
 
 _Pragma ("GCC diagnostic pop")
@@ -68,6 +78,7 @@ void RK (double t,
          double* y0,
 		 double* px0,
          double* py0,
+         double* b0,
          double h,
          unsigned int s,
          double** k,
@@ -76,6 +87,7 @@ void RK (double t,
          double g (double, double, double, double, double, double),
 		 double u (double, double, double, double, double, double),
          double v (double, double, double, double, double, double),
+         double l (double, double, double, double, double, double),
          double mult,
          bool check)
 {
@@ -83,6 +95,8 @@ void RK (double t,
     double temp_y;
 	double temp_px;
     double temp_py;
+    double temp_b;
+    
 
     for (unsigned int i = 0; i < s; i++)
     {
@@ -90,12 +104,14 @@ void RK (double t,
         temp_y = 0;
         temp_px = 0;
         temp_py = 0;
+        temp_b = 0;
         for (unsigned int j = 0; j < i; j++)
         {
             temp_x += cab[i + 1][j] * k[0][j];
             temp_y += cab[i + 1][j] * k[1][j];
 			temp_px += cab[i + 1][j] * k[2][j];
 			temp_py += cab[i + 1][j] * k[3][j];
+			temp_b += cab[i + 1][j] * k[4][j];
         }
         // printf("%f\n",f(1,0));
         k[0][i] = f (t + h * cab[0][i],
@@ -125,12 +141,23 @@ void RK (double t,
 					*px0 + h * temp_px,
 					*py0 + h * temp_py,
                     mult);
+        
+        k[4][i] = l (t + h * cab[0][i],
+					*x0 + h * temp_x,
+					*y0 + h * temp_y,
+					*px0 + h * temp_px,
+					*py0 + h * temp_py,
+                    mult);
+        
+        
     }
 
     temp_x = 0;
     temp_y = 0;
 	temp_px = 0;
     temp_py = 0;
+    temp_b = 0;
+    
 	
     if (check)
     {
@@ -140,6 +167,7 @@ void RK (double t,
             temp_y += cab[s + 2][i] * k[1][i];
 			temp_px += cab[s + 2][i] * k[2][i];
 			temp_py += cab[s + 2][i] * k[3][i];
+			temp_b += cab[s + 2][i] * k[4][i];
         }
     }
     else
@@ -150,13 +178,15 @@ void RK (double t,
             temp_y += cab[s + 1][i] * k[1][i];
 			temp_px += cab[s + 1][i] * k[2][i];
 			temp_py += cab[s + 1][i] * k[3][i];
+			temp_b += cab[s + 1][i] * k[4][i];           
         }
     }
 
     *x0 += h * temp_x;
     *y0 += h * temp_y;
 	*px0 += h * temp_px;
-    *py0 += h * temp_py;
+    *py0 += h * temp_py;      
+    *b0 += h * temp_b;       
 }
 
 // Adaptive Runge-Kutta
@@ -165,11 +195,7 @@ double astep (double T,
               double* y,
 			  double* px,
 			  double* py,
-			  
-              double* x_,
-              double* y_,
-			  double* px_,
-			  double* py_,
+			  double* b,
 			  
               long long unsigned* i,
               long long unsigned* j,
@@ -186,14 +212,17 @@ double astep (double T,
 			  double g (double, double, double, double, double, double),
 			  double u (double, double, double, double, double, double),
 			  double v (double, double, double, double, double, double),
+			  double l (double, double, double, double, double, double),
               double mult)
 {
-    double temp_x, temp_y, temp_px, temp_py, dist, h, temp, fac, err, norm;
-    *x_ = temp_x = *x;
-    *y_ = temp_y = *y;
+    double temp_x, temp_y, temp_px, temp_py, temp_b, x_, y_, px_, py_, b_, dist, h, temp, fac, err, norm;
+    x_ = temp_x = *x;
+    y_ = temp_y = *y;
     
-	*px_ = temp_px = *px;
-    *py_ = temp_py = *py;
+	px_ = temp_px = *px;
+    py_ = temp_py = *py;
+    b_ = temp_b = *b;
+    
     
     dist        = 0;
     h           = 0.01;
@@ -208,12 +237,12 @@ double astep (double T,
         
         if (dist + h > T) h = T - dist;
 
-        RK (dist, &temp_x, &temp_y, &temp_px, &temp_py, h, s, k, cab, f, g, u, v, mult, false);
-        RK (dist, x_, y_, px_, py_, h, s, k, cab, f, g, u, v, mult, true);
+        RK (dist, &temp_x, &temp_y, &temp_px, &temp_py, &temp_b, h, s, k, cab, f, g, u, v, l, mult, false);
+        RK (dist, &x_, &y_, &px_, &py_, &b_, h, s, k, cab, f, g, u, v, l, mult, true);
         
         
-        norm = fmax( fmax( fabs (temp_x - *x_), fabs (temp_y - *y_)), fmax(fabs (temp_px - *px_), fabs (temp_py - *py_)));
-        norm = sqrt(pow(temp_x - *x_,2)+pow(temp_y - *y_,2)+pow(temp_px - *px_,2)+pow(temp_py - *py_,2));
+        // norm = fmax( fmax( fabs (temp_x - x_), fabs (temp_y - y_)), fmax(fabs (temp_px - px_), fabs (temp_py - py_)));
+        norm = sqrt(pow(temp_x - x_,2)+pow(temp_y - y_,2)+pow(temp_px - px_,2)+pow(temp_py - py_,2));
         temp = h;
         h *= fmin (fac,
                    fmax (0.7,
@@ -226,12 +255,12 @@ double astep (double T,
         }
         if (norm > tol)
         {
-            *x_ = temp_x = *x;
-            *y_ = temp_y = *y;
+            x_ = temp_x = *x;
+            y_ = temp_y = *y;
 
-            *px_ = temp_px = *px;
-            *py_ = temp_py = *py;
-			
+            px_ = temp_px = *px;
+            py_ = temp_py = *py;
+			b_ = temp_b = *b;
             fac         = 1;
             *j += 1;
             continue;
@@ -243,6 +272,7 @@ double astep (double T,
         *y  = temp_y;
 		*px = temp_px;
 		*py = temp_py;
+		*b = temp_b;
         fac = 1.7;
         *i += 1;
     }
@@ -256,6 +286,7 @@ double error(double T,
 	        double y,
 	        double px,
 	        double py,
+	        double b,
 	        double* err,
 	        unsigned int p,
 	        unsigned int s,
@@ -269,22 +300,18 @@ double error(double T,
 	        double g (double, double, double, double, double, double),
 	        double u (double, double, double, double, double, double),
 	        double v (double, double, double, double, double, double),
+	        double l (double, double, double, double, double, double),
             double mult)
 {
-	double x_ = x;
-	double y_ = y;
-	double px_ = px;
-	double py_ = py;
 	long long unsigned i = 0;
 	long long unsigned j = 0;
-    double delt;
 
-	delt = astep(T, &x, &y, &px, &py, &x_, &y_, &px_, &py_, &i, &j, p, s, k, cab, tol, f, g, u, v, mult);
+	astep(T, &x, &y, &px, &py, &b, &i, &j, p, s, k, cab, tol, f, g, u, v, l, mult);
 
-	err[0] = x;
-	err[1] = py;
+	err[0] = px-0;
+	err[1] = py-0;
     
-    return delt;
+    return b;
 }
 
 /* 
@@ -324,43 +351,44 @@ void shooting_method(unsigned int max_iterations,
 	                 double g (double, double, double, double, double, double),
 	                 double u (double, double, double, double, double, double),
 	                 double v (double, double, double, double, double, double),
+	                 double l (double, double, double, double, double, double),
                      double mult)
 {
-    double alpha_1 = 1.5; //x
-    double alpha_2 = -0.5; //py
+    double alpha_1 = -1.5; //y
+    double alpha_2 = -0.5; //px
     double** A = (double**)malloc (2*sizeof(double*));
     A[0] = (double*)malloc (2*sizeof(double));
     A[1] = (double*)malloc (2*sizeof(double));
     double* B = (double*)malloc (2*sizeof(double));
     double delta = pow(10, -6);
-    double temp_alpha[2];
+    double integral = 0;
 
     for(unsigned int iteration = 0; iteration < max_iterations; iteration++){
-        error(T, x, alpha_1+delta, alpha_2, py, err, p, s, k, cab, tol, f, g, u, v, mult);
-        temp_alpha[0] = err[0];  // y
-        temp_alpha[1] = err[1];  // px
-        error(T, x, alpha_1-delta, alpha_2, py, err, p, s, k, cab, tol, f, g, u, v, mult);
-        // printf("\n%.7e    %.7e    %.7e\n",temp_alpha[0], err[0], (temp_alpha[0]-err[0])/delta);
-        // printf("%.7e    %.7e    %.7e\n\n",temp_alpha[1], err[1], (temp_alpha[1]-err[1])/delta);
-        A[0][0] = (temp_alpha[0]-err[0])*(0.5/delta);
-        A[1][0] = (temp_alpha[1]-err[1])*(0.5/delta);
+        error(T, x, alpha_1+delta, alpha_2, py, integral, err, p, s, k, cab, tol, f, g, u, v, l, mult);
+        B[0] = err[0];  // y
+        B[1] = err[1];  // px
+        error(T, x, alpha_1-delta, alpha_2, py, integral, err, p, s, k, cab, tol, f, g, u, v, l, mult);
+        // printf("\n%.7e    %.7e    %.7e\n",B[0], err[0], (B[0]-err[0])/delta);
+        // printf("%.7e    %.7e    %.7e\n\n",B[1], err[1], (B[1]-err[1])/delta);
+        A[0][0] = (B[0]-err[0])/(2*delta);
+        A[1][0] = (B[1]-err[1])/(2*delta);
 
-        error(T, x, alpha_1, alpha_2+delta, py, err, p, s, k, cab, tol, f, g, u, v, mult);
-        temp_alpha[0] = err[0];  // y
-        temp_alpha[1] = err[1];  // px
-        error(T, x, alpha_1, alpha_2-delta, py, err, p, s, k, cab, tol, f, g, u, v, mult);
-        A[0][1] = (temp_alpha[0]-err[0])*(0.5/delta);
-        A[1][1] = (temp_alpha[1]-err[1])*(0.5/delta);
+        error(T, x, alpha_1, alpha_2+delta, py, integral, err, p, s, k, cab, tol, f, g, u, v, l, mult);
+        B[0] = err[0];  // y
+        B[1] = err[1];  // px
+        error(T, x, alpha_1, alpha_2-delta, py, integral, err, p, s, k, cab, tol, f, g, u, v, l, mult);
+        A[0][1] = (B[0]-err[0])/(2*delta);
+        A[1][1] = (B[1]-err[1])*(0.5/delta);
 
-        error(T, x, alpha_1, alpha_2, py, err, p, s, k, cab, tol, f, g, u, v, mult);
+        integral = error(T, x, alpha_1, alpha_2, py, integral, err, p, s, k, cab, tol, f, g, u, v, l, mult);
         B[0] = err[0];  //y
         B[1] = err[1];  //px
         
         // printf("%10.2e    %10.2e  |  %10.2e\n%10.2e    %10.2e  |  %10.2e\n", A[0][0], A[0][1], B[0], A[1][0], A[1][1], B[1]);
         
-        if(sqrt(pow(B[0],2)+pow(B[1],2)) < tol*pow(10,3)){
+        if(sqrt(pow(B[0],2)+pow(B[1],2)) < tol*pow(10,2)){
             //printf and break
-            printf("Shooting iteration: %ud,\nAlpha: %.3f,\nX(0)=%.14f,\nY(0)=%.14f,\nDiscrepancy: %.3e\n\n", iteration, mult,alpha_1,alpha_2,sqrt(pow(B[0],2)+pow(B[1],2)));
+            printf("Shooting iteration: %u,\nAlpha: %.3f,\nY(0)=%.14f,\nPx(0)=%.14f,\nB=%.14f,\nDiscrepancy: %.3e\n\n", iteration, mult, alpha_1, alpha_2, integral, sqrt(pow(B[0], 2) + pow(B[1], 2)));
 
             free (A[0]);
             free (A[1]);
@@ -374,8 +402,7 @@ void shooting_method(unsigned int max_iterations,
 
         alpha_1 -= (A[0][0]*B[0]-A[1][0]*B[1])/(A[1][0]*A[0][1]-A[0][0]*A[1][1]);
         alpha_2 += (A[0][0]*B[1]-A[1][0]*B[0])/(A[1][0]*A[0][1]-A[0][0]*A[1][1]);
-                
-        //changing alpha_1, alpha_2
+        integral = 0;
     }
     free (A[0]);
     free (A[1]);
@@ -394,11 +421,7 @@ int main()
 	double y=0;
 	double px=1;
 	double py=0;
-    
-    double x_;
-	double y_;
-	double px_;
-	double py_;
+    double b=0;
 	
     double*  mult; // alpha из условия
     double* alpha; // под невязки
@@ -416,7 +439,7 @@ int main()
 
     time (&start);
 
-    FILE* inpf = fopen ("koef (8).txt", "r");
+    FILE* inpf = fopen ("koef (8).txt", "r"); //207
     if (inpf == NULL)
     {
         printf ("File doen`t exist\n");
@@ -450,14 +473,15 @@ int main()
     mult[3] = 1;
 
 	alpha = (double*)malloc (2 * sizeof (double));
-    alpha[0] = 1;
-    alpha[1] = 1;
+    alpha[0] = 0;
+    alpha[1] = 0;
 
-    k    = (double**)malloc (4 * sizeof (double*));
+    k    = (double**)malloc (5 * sizeof (double*));
     k[0] = (double*)malloc (s * sizeof (double));
     k[1] = (double*)malloc (s * sizeof (double));
 	k[2] = (double*)malloc (s * sizeof (double));
     k[3] = (double*)malloc (s * sizeof (double));
+    k[4] = (double*)malloc (s * sizeof (double));
 	
     for (i = 0; i < s; i++)
     {
@@ -465,6 +489,7 @@ int main()
         k[1][i] = 0;
 		k[2][i] = 0;
         k[3][i] = 0;
+        k[4][i] = 0;
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////
@@ -494,8 +519,8 @@ int main()
     ///////////////////////////////////////////////////////////////////////////////////////// 
     
     // the shooting method
-	astep(pi*pow(10,3), &x, &y, &px, &py, &x_, &y_, &px_, &py_, &i, &j, p, s, k, cab, tol, test_x_d, test_y_d, test_px_d, test_py_d, 0);
-    printf("The Runge-Kutta test:\n%.2e    %.2e  |  %.2e    %.2e  |  10^3*Pi\n\n",x-1,y,px-1,py);
+	astep(pi*pow(10,3), &x, &y, &px, &py, &b, &i, &j, p, s, k, cab, tol, test_x_d, test_y_d, test_px_d, test_py_d, test_B_d, 0);
+    printf("The Runge-Kutta test:\n%.2e    %.2e  |  %.2e    %.2e  |  %.7e  |  10^3*Pi\n\n", x - 1, y, px - 1, py, b);
     
 	x=1;
 	y=0;
@@ -503,7 +528,7 @@ int main()
 	py=0;
     
     for(unsigned int l = 0; l < 4; l++)
-        shooting_method(1500, T, x, py, alpha, p, s, k, cab, tol, x_d, y_d, px_d, py_d, mult[l]);
+        shooting_method(1500, T, x, py, alpha, p, s, k, cab, tol, x_d, y_d, px_d, py_d, B_d, mult[l]);
 
     /////////////////////////////////////////////////////////////////////////////////////////
 
